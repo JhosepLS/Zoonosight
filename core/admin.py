bb900302924a85e98b42e96348215f701b1ae453
JhosepLS <jmollapazam@ulasalle.edu.pe>
2025-11-07 18:23:10 -0500

    Añadiendo el contenido de admin.py y Actualizando requirements

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import Departamento, Provincia, Distrito, TipoZoonosis, Paciente, Caso
 
-# Register your models here.
+@admin.register(Departamento)
+class DepartamentoAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'codigo_ubigeo', 'poblacion']
+    search_fields = ['nombre']
+
+@admin.register(Provincia)
+class ProvinciaAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'departamento', 'codigo_ubigeo']
+    list_filter = ['departamento']
+    search_fields = ['nombre']
+
+@admin.register(Distrito)
+class DistritoAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'provincia', 'codigo_ubigeo']
+    list_filter = ['provincia__departamento']
+    search_fields = ['nombre']
+
+@admin.register(TipoZoonosis)
+class TipoZoonosisAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'codigo_cie10', 'animal_vector']
+    search_fields = ['nombre']
+
+@admin.register(Paciente)
+class PacienteAdmin(admin.ModelAdmin):
+    list_display = ['id', 'edad', 'tipo_edad', 'grupo_etario', 'genero']
+    list_filter = ['grupo_etario', 'genero']
+
+@admin.register(Caso)
+class CasoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'zoonosis', 'distrito', 'anio', 'semana_epidemiologica', 'tipo_diagnostico']
+    list_filter = ['anio', 'zoonosis', 'tipo_diagnostico']
+    search_fields = ['distrito__nombre']
+    date_hierarchy = 'fecha_notificacion'
